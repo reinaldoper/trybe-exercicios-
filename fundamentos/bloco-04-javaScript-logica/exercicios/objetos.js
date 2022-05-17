f790499a707b70aa882399b2c1d884dc7e2a823b
Reinaldo <reinaldoper83@gmail.com>
2022-05-17 16:28:59 -0300

    acessando o valor pelo inndice usando forIn

--- fundamentos/bloco-04-javaScript-logica/exercicios/objetos.js
+++ fundamentos/bloco-04-javaScript-logica/exercicios/objetos.js
@@ -16,3 +16,45 @@ console.log("");
 
 //terceiro exercicio de fixação de objetos
 console.log("A jogadora possui "+player.medals.golden+ " medalhas de ouro e "+player.medals.silver+ " medalhas de prata.");
+console.log("");
+//exercicios forIn
+let cars = ['Saab', 'Volvo', 'BMW'];
+
+for (let index in cars) {
+  console.log(cars[index]);
+}
+console.log("");
+// outro exercicios forIn
+let car = {
+    type: 'Fiat',
+    model: '500',
+    color: 'white',
+  };
+  
+  for (let index in car) {
+    console.log(index, car[index]);
+  }
+  console.log("");
+  //outro exercicios de fixação
+  let food = ['hamburguer', 'bife', 'acarajé'];
+  for (let position in food) {
+    console.log(position);
+  };
+  //resultado: 0, 1, 2;
+  console.log("");
+  //exemplo forOf
+ food = ['hamburguer', 'bife', 'acarajé'];
+for (let value of food) {
+  console.log(value);
+};
+//resultado: hamburguer, bife, acarajé;
+console.log("");
+//exercicios de fixação
+let names = {
+    person1: 'João',
+    person2: 'Maria',
+    person3: 'Jorge',
+  };
+  for(let key in names){
+    console.log("Olá "+names[key]);
+  }
