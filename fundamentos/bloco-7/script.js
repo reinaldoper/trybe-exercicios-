d6598f81a6e4516e546f6c0a52a6300d3d2a72ef
Reinaldo <reinaldoper83@gmail.com>
2022-06-13 20:22:41 -0300

    exercicio segunda parte de objetos

--- fundamentos/bloco-7/script.js
+++ fundamentos/bloco-7/script.js
@@ -247,4 +247,60 @@ const person = {
   const newPerson = Object.assign({},person,lastName);
   newPerson.name = 'Gilberto';
   console.log(newPerson);
-  console.log(person);
+console.log(person);
+  
+
+
+//exercicios do course
+
+  const order = {
+    name: 'Rafael Andrade',
+    phoneNumber: '11-98763-1416',
+    address: {
+      street: 'Rua das Flores',
+      number: '389',
+      apartment: '701',
+    },
+    order: {
+      pizza: {
+        marguerita: {
+          amount: 1,
+          price: 25,
+        },
+        pepperoni: {
+          amount: 1,
+          price: 20,
+        }
+      },
+      drinks: {
+        coke: {
+          type: 'Coca-Cola Zero',
+          price: 10,
+          amount: 1,
+        }
+      },
+      delivery: {
+        deliveryPerson: 'Ana Silveira',
+        price: 5,
+      }
+    },
+    payment: {
+      total: 60,
+    },
+  };
+
+const customerInfo = (order) => {
+  //"Olá Ana Silveira, entrega para: Rafael Andrade, Telefone: 11-98763-1416, R. Rua das Flores, Nº: 389, AP: 701".
+    
+    return `Olá  ${order.order.delivery.deliveryPerson}, entrega para: ${order.name}, telefone:${order.phoneNumber}, R. ${order.address.street}, N°: ${order.address.number}, AP: ${order.address.apartment}.`;
+  }
+  console.log(customerInfo(order));
+  
+ 
+  
+  const orderModifier = (order) => {
+    // Adicione abaixo as informações necessárias.
+  
+  }
+  
+  orderModifier(order);
