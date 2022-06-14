4cc2a1a400bcc3b3cb8c0bfa577e6bf37ccdb81a
Reinaldo <reinaldoper83@gmail.com>
2022-06-14 13:14:00 -0300

    exercicios da terceira parte de objetos

--- fundamentos/bloco-7/script.js
+++ fundamentos/bloco-7/script.js
@@ -1,311 +1,373 @@
-
 //tratamento de exceções
 const sum = (value1, value2) => {
-    if (typeof value1 !== 'number' || typeof value2 !== 'number') {
-      return 'Os valores devem ser numéricos';
-    }
-    return value1 + value2;
-  };
-  
-  console.log(sum(2, '3'));
+  if (typeof value1 !== 'number' || typeof value2 !== 'number') {
+    return 'Os valores devem ser numéricos';
+  }
+  return value1 + value2;
+};
 
-  //exemplo 2
+console.log(sum(2, '3'));
 
-  const sum = (value1, value2) => {
-    if (typeof value1 !== 'number' || typeof value2 !== 'number') {
-      throw new Error('Os valores devem ser numéricos');
-    }
+//exemplo 2
+
+const sum = (value1, value2) => {
+  if (typeof value1 !== 'number' || typeof value2 !== 'number') {
+    throw new Error('Os valores devem ser numéricos');
+  }
+  return value1 + value2;
+};
+
+console.log(sum(2, '3'));
+//try-cath
+const verifyIsNumber = (value1, value2) => {
+  if (typeof value1 !== 'number' || typeof value2 !== 'number') {
+    throw new Error('Os valores devem ser numéricos');
+  }
+};
+
+const sum = (value1, value2) => {
+  try {
+    verifyIsNumber(value1, value2);
     return value1 + value2;
-  };
-  
+  } catch (error) {
+    return error.message;
+  }
+};
+
 console.log(sum(2, '3'));
-  //try-cath
-  const verifyIsNumber = (value1, value2) => {
-    if (typeof value1 !== 'number' || typeof value2 !== 'number') {
-      throw new Error('Os valores devem ser numéricos');
-    }
-  };
-  
-  const sum = (value1, value2) => {
-    try {
-      verifyIsNumber(value1, value2);
-      return value1 + value2;
-    } catch (error) {
-      return error.message;
-    }
-  };
-  
-console.log(sum(2, '3'));
-  
+
 //acessar a propriedade do objeto
 const customer1 = {
-    firstName: 'Roberto',
-    age: 22,
-    job: 'Teacher',
-  };
-  
-  console.log(customer1);
-  
-  customer1.lastName = 'Faria';
-  console.log(customer1);
-  
-  const customer2 = {
-    firstName: 'Maria',
-    age: 23,
-    job: 'Medic',
-  };
-  
-  console.log(customer2);
-  customer2['lastName'] = 'Silva';
+  firstName: 'Roberto',
+  age: 22,
+  job: 'Teacher',
+};
+
+console.log(customer1);
+
+customer1.lastName = 'Faria';
+console.log(customer1);
+
+const customer2 = {
+  firstName: 'Maria',
+  age: 23,
+  job: 'Medic',
+};
+
+console.log(customer2);
+customer2['lastName'] = 'Silva';
 console.log(customer2);
 
-  //adicionando chave e valor
+//adicionando chave e valor
 const customer = {
-    firstName: 'Roberto',
-    age: 22,
-    job: 'Teacher',
-  };
-  
-  let newKey = 'lastName';
-  const lastName = 'Ferreira';
-  customer[newKey] = lastName;
-  newKey = 'fullName';
-  const fullName = `${customer.firstName} ${customer.lastName}`;
-  customer[newKey] = fullName;
+  firstName: 'Roberto',
+  age: 22,
+  job: 'Teacher',
+};
+
+let newKey = 'lastName';
+const lastName = 'Ferreira';
+customer[newKey] = lastName;
+newKey = 'fullName';
+const fullName = `${customer.firstName} ${customer.lastName}`;
+customer[newKey] = fullName;
 console.log(customer);
-  
+
 //listar propriedades do objeto
 
 const coolestTvShow = {
-    name: "BoJack Horseman",
-    genre: "adult animation",
-    author: "Raphael Bob-Waksberg",
-    favoriteCharacter: "Princess Carolyn",
-    quote: "Princess Carolyn always lands on her feet.",
-    seasons: 6,
-  };
-  
-  for (const property in coolestTvShow) {
-    console.log(property);
+  name: "BoJack Horseman",
+  genre: "adult animation",
+  author: "Raphael Bob-Waksberg",
+  favoriteCharacter: "Princess Carolyn",
+  quote: "Princess Carolyn always lands on her feet.",
+  seasons: 6,
+};
+
+for (const property in coolestTvShow) {
+  console.log(property);
+}
+
+// name
+// genre
+// author
+// favoriteCharacter
+// quote
+// seasons
+
+//utilizando a função keys
+const coolestTvShow = {
+  name: "BoJack Horseman",
+  genre: "adult animation",
+  createdBy: "Raphael Bob-Waksberg",
+  favoriteCharacter: "Princess Carolyn",
+  quote: "Princess Carolyn always lands on her feet.",
+  seasons: 6,
+};
+
+// for (const property in coolestTvShow) {
+//   console.log(property);
+// }
+
+console.log(Object.keys(coolestTvShow));
+
+// [ 'name', 'genre', 'createdBy', 'favoriteCharacter', 'quote', 'seasons' ]
+
+const student1 = {
+  Html: 'Muito Bom',
+  Css: 'Bom',
+  JavaScript: 'Ótimo',
+  SoftSkills: 'Ótimo',
+};
+
+const student2 = {
+  Html: 'Bom',
+  Css: 'Ótimo',
+  JavaScript: 'Ruim',
+  SoftSkills: 'Ótimo',
+  Git: 'Bom', // chave adicionada
+};
+
+const listSkills = (student) => {
+  const arrayOfSkills = Object.keys(student);
+  for (const index in arrayOfSkills) {
+    console.log(`${arrayOfSkills[index]}, Nível: ${student[arrayOfSkills[index]]}`);
   }
-  
-  // name
-  // genre
-  // author
-  // favoriteCharacter
-  // quote
-  // seasons
+};
 
-  //utilizando a função keys
-  const coolestTvShow = {
-    name: "BoJack Horseman",
-    genre: "adult animation",
-    createdBy: "Raphael Bob-Waksberg",
-    favoriteCharacter: "Princess Carolyn",
-    quote: "Princess Carolyn always lands on her feet.",
-    seasons: 6,
-  };
-  
-  // for (const property in coolestTvShow) {
-  //   console.log(property);
-  // }
-  
-  console.log(Object.keys(coolestTvShow));
-  
-  // [ 'name', 'genre', 'createdBy', 'favoriteCharacter', 'quote', 'seasons' ]
+console.log('Estudante 1');
+listSkills(student1);
 
-  const student1 = {
-    Html: 'Muito Bom',
-    Css: 'Bom',
-    JavaScript: 'Ótimo',
-    SoftSkills: 'Ótimo',
-  };
-  
-  const student2 = {
-    Html: 'Bom',
-    Css: 'Ótimo',
-    JavaScript: 'Ruim',
-    SoftSkills: 'Ótimo',
-    Git: 'Bom', // chave adicionada
-  };
-  
-  const listSkills = (student) => {
-    const arrayOfSkills = Object.keys(student);
-    for (const index in arrayOfSkills) {
-      console.log(`${arrayOfSkills[index]}, Nível: ${student[arrayOfSkills[index]]}`);
-    }
-  };
-  
-  console.log('Estudante 1');
-  listSkills(student1);
-  
-  console.log('Estudante 2');
+console.log('Estudante 2');
 listSkills(student2);
-  
+
 //pegando o valor do objeto
 const student = {
-    Html: 'Muito Bom',
-    Css: 'Bom',
-    JavaScript: 'Ótimo',
-    SoftSkill: 'Ótimo',
-  };
-  
-  const listSkillsValuesWithFor = (student) => {
-    const skills = [];
-    for(skill in student) {
-      skills.push(student[skill]);
-    }
-  
-    return skills;
-  };
-  
-  const listSkillsValuesWithValues = (student) => Object.values(student);
-  
-  // Sem Object.values
-  console.log(listSkillsValuesWithFor(student));
-  
-  // Com Object.values
+  Html: 'Muito Bom',
+  Css: 'Bom',
+  JavaScript: 'Ótimo',
+  SoftSkill: 'Ótimo',
+};
+
+const listSkillsValuesWithFor = (student) => {
+  const skills = [];
+  for (skill in student) {
+    skills.push(student[skill]);
+  }
+
+  return skills;
+};
+
+const listSkillsValuesWithValues = (student) => Object.values(student);
+
+// Sem Object.values
+console.log(listSkillsValuesWithFor(student));
+
+// Com Object.values
 console.log(listSkillsValuesWithValues(student));
-  
+
 //chave e valor
 
 const coolestTvShow = {
-    name: "BoJack Horseman",
-    genre: "adult animation",
-    createdBy: "Raphael Bob-Waksberg",
-    favoriteCharacter: "Princess Carolyn",
-    quote: "Princess Carolyn always lands on her feet.",
-    seasons: 6,
-  };
-  
-  console.log(Object.entries(coolestTvShow));
-  
-  // [
-  //   [ 'name', 'BoJack Horseman' ],
-  //   [ 'genre', 'adult animation' ],
-  //   [ 'createdBy', 'Raphael Bob-Waksberg' ],
-  //   [ 'favoriteCharacter', 'Princess Carolyn' ],
-  //   [ 'quote', 'Princess Carolyn always lands on her feet.' ],
-  //   [ 'seasons', 6 ]
-  // ]
-  const países = {
-    França: 'Paris',
-    Brasil: 'Brasília',
-    Espanha: 'Madrid',
-    Portugal: 'Lisboa',
-  };
-  const pairKeyValue = Object.entries(países);
-console.log(pairKeyValue);
-  
-for(index in pairKeyValue) {
-    console.log('--------');
-    console.log('País:', pairKeyValue[index][0]);
-    console.log('Capital:', pairKeyValue[index][1]);
+  name: "BoJack Horseman",
+  genre: "adult animation",
+  createdBy: "Raphael Bob-Waksberg",
+  favoriteCharacter: "Princess Carolyn",
+  quote: "Princess Carolyn always lands on her feet.",
+  seasons: 6,
 };
-  
-  //assign
-  const person = {
-    name: 'Alberto',
-    lastName: 'Gomes',
-    age: 20,
-  };
-  
-  const info = {
-    age: 23,
-    job: 'engenheiro',
-  };
-  
-  const family = {
-    children: ['Maria', 'João'],
-    wife: 'Ana',
-  };
-  
-  Object.assign(person, info, family)
-  console.log(person)
-  
-  /* Output
-    { name: 'Alberto',
-    lastName: 'Gomes',
-    age: 23,
-    job: 'engenheiro',
-    children: [ 'Maria', 'João' ],
-    wife: 'Ana'
-    } */
 
-    const obj = { value1: 10, value2: 11 };
+console.log(Object.entries(coolestTvShow));
+
+// [
+//   [ 'name', 'BoJack Horseman' ],
+//   [ 'genre', 'adult animation' ],
+//   [ 'createdBy', 'Raphael Bob-Waksberg' ],
+//   [ 'favoriteCharacter', 'Princess Carolyn' ],
+//   [ 'quote', 'Princess Carolyn always lands on her feet.' ],
+//   [ 'seasons', 6 ]
+// ]
+const países = {
+  França: 'Paris',
+  Brasil: 'Brasília',
+  Espanha: 'Madrid',
+  Portugal: 'Lisboa',
+};
+const pairKeyValue = Object.entries(países);
+console.log(pairKeyValue);
+
+for (index in pairKeyValue) {
+  console.log('--------');
+  console.log('País:', pairKeyValue[index][0]);
+  console.log('Capital:', pairKeyValue[index][1]);
+};
+
+//assign
+const person = {
+  name: 'Alberto',
+  lastName: 'Gomes',
+  age: 20,
+};
+
+const info = {
+  age: 23,
+  job: 'engenheiro',
+};
+
+const family = {
+  children: ['Maria', 'João'],
+  wife: 'Ana',
+};
+
+Object.assign(person, info, family)
+console.log(person)
+
+/* Output
+  { name: 'Alberto',
+  lastName: 'Gomes',
+  age: 23,
+  job: 'engenheiro',
+  children: [ 'Maria', 'João' ],
+  wife: 'Ana'
+  } */
+
+const obj = {
+  value1: 10,
+  value2: 11
+};
 const cloneObj = obj;
-    
+
 
 const person = {
-    name:'Roberto',
-  };
-  
-  const lastName = {
-    lastName: 'Silva',
-  };
-  
-  const newPerson = Object.assign({},person,lastName);
-  newPerson.name = 'Gilberto';
-  console.log(newPerson);
+  name: 'Roberto',
+};
+
+const lastName = {
+  lastName: 'Silva',
+};
+
+const newPerson = Object.assign({}, person, lastName);
+newPerson.name = 'Gilberto';
+console.log(newPerson);
 console.log(person);
-  
+
 
 
 //exercicios do course
 
-  const order = {
-    name: 'Rafael Andrade',
-    phoneNumber: '11-98763-1416',
-    address: {
-      street: 'Rua das Flores',
-      number: '389',
-      apartment: '701',
-    },
-    order: {
-      pizza: {
-        marguerita: {
-          amount: 1,
-          price: 25,
-        },
-        pepperoni: {
-          amount: 1,
-          price: 20,
-        }
+const order = {
+  name: 'Rafael Andrade',
+  phoneNumber: '11-98763-1416',
+  address: {
+    street: 'Rua das Flores',
+    number: '389',
+    apartment: '701',
+  },
+  order: {
+    pizza: {
+      marguerita: {
+        amount: 1,
+        price: 25,
       },
-      drinks: {
-        coke: {
-          type: 'Coca-Cola Zero',
-          price: 10,
-          amount: 1,
-        }
-      },
-      delivery: {
-        deliveryPerson: 'Ana Silveira',
-        price: 5,
+      pepperoni: {
+        amount: 1,
+        price: 20,
       }
     },
-    payment: {
-      total: 60,
+    drinks: {
+      coke: {
+        type: 'Coca-Cola Zero',
+        price: 10,
+        amount: 1,
+      }
     },
-  };
+    delivery: {
+      deliveryPerson: 'Ana Silveira',
+      price: 5,
+    }
+  },
+  payment: {
+    total: 60,
+  },
+};
 
 const customerInfo = (order) => {
   //"Olá Ana Silveira, entrega para: Rafael Andrade, Telefone: 11-98763-1416, R. Rua das Flores, Nº: 389, AP: 701".
-    
-    return `Olá  ${order.order.delivery.deliveryPerson}, entrega para: ${order.name}, telefone:${order.phoneNumber}, R. ${order.address.street}, N°: ${order.address.number}, AP: ${order.address.apartment}.`;
-  }
-  console.log(customerInfo(order));
-  
- 
-  
-  const orderModifier = (order) => {
-    // "Olá Luiz Silva, o total do seu pedido de marguerita, pepperoni e Coca-Cola Zero é R$ 50,00."
-    order.order.delivery.deliveryPerson = 'Luiz Silva';
-    order.payment.total = '50';
 
-    return `Olá: ${order.order.delivery.deliveryPerson}, o total do seu pedido de marguerita, pepperoni e coca-cola Zero é R$ ${order.payment.total}.` ;
-    
-  
-  }
-  console.log(orderModifier(order));
-  /* orderModifier(order); */
+  return `Olá  ${order.order.delivery.deliveryPerson}, entrega para: ${order.name}, telefone:${order.phoneNumber}, R. ${order.address.street}, N°: ${order.address.number}, AP: ${order.address.apartment}.`;
+}
+console.log(customerInfo(order));
+
+
+
+const orderModifier = (order) => {
+  // "Olá Luiz Silva, o total do seu pedido de marguerita, pepperoni e Coca-Cola Zero é R$ 50,00."
+  order.order.delivery.deliveryPerson = 'Luiz Silva';
+  order.payment.total = '50';
+
+  return `Olá: ${order.order.delivery.deliveryPerson}, o total do seu pedido de marguerita, pepperoni e coca-cola Zero é R$ ${order.payment.total}.`;
+
+
+}
+console.log(orderModifier(order));
+/* orderModifier(order); */
+
+
+
+//exercicios da terceira parte do course sobre objetos
+const lesson1 = {
+  materia: 'Matemática',
+  numeroEstudantes: 20,
+  professor: 'Maria Clara',
+  turno: 'manhã',
+};
+
+const lesson2 = {
+  materia: 'História',
+  numeroEstudantes: 20,
+  professor: 'Carlos',
+};
+
+const lesson3 = {
+  materia: 'Matemática',
+  numeroEstudantes: 10,
+  professor: 'Maria Clara',
+  turno: 'noite',
+};
+ 
+//1. Crie uma função para adicionar o turno da noite na lesson2. Essa função deve possuir três parâmetros, sendo eles: o objeto a ser modificado, a chave que deverá ser adicionada e o valor dela.
+
+ function addLesson2(obj, key, value) {
+  let newKey = key;
+  obj[newKey] = value;
+  console.log(obj);
+}
+console.log(addLesson2(lesson2, key, value));
+
+//2.Crie uma função para listar as keys de um objeto. Essa função deve receber um objeto como parâmetro.
+function listaKeys(obj) {
+  return Object.keys(obj);
+}
+
+//3.Crie uma função para mostrar o tamanho de um objeto.
+function tamanhoObj(obj) {
+  return Object.keys(obj).length;
+}
+
+//4.Crie uma função para listar os valores de um objeto. Essa função deve receber um objeto como parâmetro.
+function valoresObj(obj) {
+  return Object.values(obj);
+}
+//5.Crie um objeto de nome allLessons, que deve agrupar todas as aulas através do Object.assign. Cada chave desse novo objeto será uma aula, sendo essas chaves: lesson1, lesson2 e lesson3. Ao executar o comando console.log(allLessons), a saída deverá ser a seguinte:
+
+const alls = Object.assign(lesson1)
+const alls1 = Object.assign(lesson2)
+const alls2 = Object.assign(lesson3)
+
+const allLessons = {
+  lesson1: alls,
+  lesson2: alls1,
+  lesson3: alls2,
+};
+;
+console.log(Object.assign(allLessons));
