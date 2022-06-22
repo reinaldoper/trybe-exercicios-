588fca2d700fd5825097c67ba1e7c468e0e53f8d
Reinaldo <reinaldoper83@gmail.com>
2022-06-22 20:06:34 -0300

    melhorando requisito 2

--- fundamentos/bloco-8/script.js
+++ fundamentos/bloco-8/script.js
@@ -33,17 +33,16 @@ console.log(newEmployees(cadastro));
 
 const sorteio = (num, callback) => {
     let result = Math.floor(Math.random() * 5 + 1);
-    callback(result, num);
-    console.log(callback(result, num));
     return callback(result, num);
 
 }
 
-const verifica = (result, num) => {
-    if (result === num) {
-        return 'Parabéns você ganhou';
-    } else {
-        return 'Tente novamente';
-    }
-}
-console.log(sorteio(3, verifica));
+const verifica = (result, num) => (result === num) ? 'Parabéns você ganhou' : 'Tente novamente'; 
+console.log(sorteio(4, verifica));
+//Crie uma HOF que receberá três parâmetros. O primeiro será um array de respostas corretas (Gabarito), o segundo será um array de respostas a serem verificadas (respostas da pessoa estudante) e o terceiro é uma função que checa se as respostas estão corretas e faz a contagem da pontuação final recebida pela pessoa estudante. Ao final a HOF deve retornar o total da contagem de respostas certas.
+//Quando a resposta for correta a contagem sobe 1 ponto, quando for incorreta desce 0.5 pontos, e quando não houver resposta ("N.A") não altera-se a contagem.
+const RIGHT_ANSWERS = ['A', 'C', 'B', 'D', 'A', 'A', 'D', 'A', 'D', 'C'];
+const STUDENT_ANSWERS = ['A', 'N.A', 'B', 'D', 'A', 'C', 'N.A', 'A', 'D', 'B'];
+
+const 
+
