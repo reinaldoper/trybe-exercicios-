4ebdc1cf02c038acda8d6b76bfbc6971c463f1b5
Reinaldo <reinaldoper83@gmail.com>
2022-06-22 19:15:18 -0300

    exercicio de numero aleatório

--- fundamentos/bloco-8/script.js
+++ fundamentos/bloco-8/script.js
@@ -30,3 +30,20 @@ const cadastro = (name) => {
 console.log(newEmployees(cadastro));
 
 //Desenvolva uma HOF que retorna o resultado de um sorteio. Esta HOF irá gerar um número aleatório entre 1 e 5 recebendo como parâmetros o número apostado e uma função que checa se o número apostado é igual ao número sorteado. O retorno da sua HOF deve ser uma string (Ex: "Tente novamente" ou "Parabéns você ganhou").
+
+const sorteio = (num, callback) => {
+    let result = Math.floor(Math.random() * 5 + 1);
+    callback(result, num);
+    console.log(callback(result, num));
+    return callback(result, num);
+
+}
+
+const verifica = (result, num) => {
+    if (result === num) {
+        return 'Parabéns você ganhou';
+    } else {
+        return 'Tente novamente';
+    }
+}
+console.log(sorteio(3, verifica));
