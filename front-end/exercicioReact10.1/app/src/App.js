91119adc02eee8101d27758b56b00954e22b7672
Reinaldo <reinaldoper83@gmail.com>
2022-07-18 19:58:46 -0300

    exercicios 3 e 4 do course

--- front-end/exercicioReact10.1/app/src/App.js
+++ front-end/exercicioReact10.1/app/src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Header from './Header';
+import Content from './Content';
+import Footer from './Footer';
 const Task = (value) => {
   return (
-    <li>{value}</li>
+    <li key={value}>{value}</li>
   );
 }
 const compromissos = ['Viajar', 'trabalhar', 'estudar'];
@@ -12,9 +14,16 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+      <Header/>
         <img src={logo} className="App-logo" alt="logo" />
         <ol>{myList}</ol>
       </header>
+      <div className='div'>
+      <Content/>
+      </div>
+      <footer className='footer'>
+        <Footer/>
+      </footer>
     </div>
   );
 }
