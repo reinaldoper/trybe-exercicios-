f91afd2af356d6b06b9adc6132e6cd3d6d660708
Reinaldo <reinaldoper83@gmail.com>
2022-07-21 18:55:17 -0300

    exercicio 2 de fixação

--- front-end/bloco-11/eventos/fancy-buttons/src/App.js
+++ front-end/bloco-11/eventos/fancy-buttons/src/App.js
@@ -1,26 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
-
-function handleClick() {
-  console.log('Clicou no botão1!')
-}
-function handleClicks() {
-  console.log('Clicou no botão2!')
-}
-function handleCli() {
-  console.log('Clicou no botão3!')
-}
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <button onClick={handleClick}>Meu botão1</button>
-        <button onClick={handleClicks}>Meu botão2</button>
-        <button onClick={handleCli}>Meu botão3</button>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react';
+class App extends Component {
+  constructor() {
+    super()
+    // A função abaixo vincula "manualmente" o `this` à nossa função
+    this.handleClick = this.handleClick.bind(this)
+    this.handleClicks = this.handleClicks.bind(this)
+    this.handleCli= this.handleCli.bind(this)
+  }
+  handleClick = () => {
+    console.log('Clicou no botão1!')
+  }
+  handleClicks = () => {
+    console.log('Clicou no botão2!')
+  }
+  handleCli = () => {
+    console.log('Clicou no botão3!')
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <button onClick={this.handleClick}>Meu botão1</button>
+          <button onClick={this.handleClicks}>Meu botão2</button>
+          <button onClick={this.handleCli}>Meu botão3</button>
+        </header>
+      </div>
+    );
+  }
+  
 }
 
 export default App;
