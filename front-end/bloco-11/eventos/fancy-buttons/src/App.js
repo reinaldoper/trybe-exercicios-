0890c914569cc4fd7e8d72b377c10ebfa8d58806
Reinaldo <reinaldoper83@gmail.com>
2022-07-21 19:11:28 -0300

    exercicios de fixação 3

--- front-end/bloco-11/eventos/fancy-buttons/src/App.js
+++ front-end/bloco-11/eventos/fancy-buttons/src/App.js
@@ -4,13 +4,18 @@ import React, { Component } from 'react';
 class App extends Component {
   constructor() {
     super()
+    this.state = {
+      numeroDeCliques: 0
+    }
     // A função abaixo vincula "manualmente" o `this` à nossa função
     this.handleClick = this.handleClick.bind(this)
     this.handleClicks = this.handleClicks.bind(this)
     this.handleCli= this.handleCli.bind(this)
   }
   handleClick = () => {
-    console.log('Clicou no botão1!')
+    this.setState((estadoAnterior, _props) => ({
+      numeroDeCliques: estadoAnterior.numeroDeCliques + 1
+    }))
   }
   handleClicks = () => {
     console.log('Clicou no botão2!')
@@ -23,7 +28,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <button onClick={this.handleClick}>Meu botão1</button>
+          <button onClick={this.handleClick}>{this.state.numeroDeCliques}</button>
           <button onClick={this.handleClicks}>Meu botão2</button>
           <button onClick={this.handleCli}>Meu botão3</button>
         </header>
