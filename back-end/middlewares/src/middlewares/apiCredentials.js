ef2ff6eb2172e71a8c4a88f67003271d709a5c2c
Reinaldo <reinaldoper83@gmail.com>
2022-11-06 05:17:02 -0300

    adicionando clone trybeCar

--- back-end/middlewares/src/middlewares/apiCredentials.js
+++ back-end/middlewares/src/middlewares/apiCredentials.js
@@ -1,14 +1,25 @@
 const fs = require('fs/promises');
 
+const geraStringAleatoria = () => {
+  let stringAleatoria = '';
+  const caracteres = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  for (let i = 0; i < 12; i++) {
+      stringAleatoria += caracteres.charAt(Math.floor(Math.random() * caracteres.length));
+  }
+  return stringAleatoria;
+};
+
+/* console.log(geraStringAleatoria()); */
 // como vamos ler arquivos assincronamente, precisamos do async
 module.exports = async function apiCredentials(req, res, next) {
   // pega o token do cabeçalho, se houver
   const token = req.header('X-API-TOKEN');
+  
   // lê o conteúdo do `./authdata.json` (relativo à raiz do projeto)
   const authdata = await fs.readFile('./authdata.json', { encoding: 'utf-8' });
   // readFile nos deu uma string, agora vamos carregar um objeto a partir dela
+  
   const authorized = JSON.parse(authdata);
-
   if (token in authorized) {
     req.teams = authorized[token];
     next(); // pode continuar
@@ -16,3 +27,4 @@ module.exports = async function apiCredentials(req, res, next) {
     res.sendStatus(401); // não autorizado
   }
 };
+module.exports = geraStringAleatoria;
