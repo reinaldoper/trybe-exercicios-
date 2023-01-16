c20ec55a52858d54804e3d165ceae577b7c4cce0
Reinaldo <reinaldoper83@gmail.com>
2023-01-16 16:37:58 -0300

    fix: exercicios do course para fixação

---
+++ back-end/poo-solid/src/EmailNotification.ts
@@ -0,0 +1,19 @@
+// EmailNotification.ts
+
+// eslint-disable-next-line import/no-unresolved, import/extensions
+import Notificator from './Notificator';
+
+export default class EmailNotification implements Notificator {
+  private email: string;
+
+  constructor(email: string) {
+    this.email = email;
+  }
+
+  sendNotification(message: string): void {
+    console.log(
+      `Here should go the implementation \
+      to send notification to the email: ${this.email} - ${message}`,
+    );
+  }
+}
