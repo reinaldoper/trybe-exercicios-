ecf575601f4eddd012e7a401ae49332443bad88f
reinaldoper <reinaldoper83@gmail.com>
2023-05-11 15:12:06 -0400

    feat: hash

---
+++ ciencia-computacao/hash-map/.hash/colisao.py
@@ -0,0 +1,23 @@
+class Employee:
+    def __init__(self, id_num, name):
+        self.id_num = id_num
+        self.name = name
+
+
+class HashMap:
+    def __init__(self):
+        self._buckets = [[] for i in range(10)]
+
+    def get_address(self, id_num):
+        return id_num % 10
+
+    def insert(self, employee):
+        address = self.get_address(employee.id_num)
+        self._buckets[address].append(employee)
+
+    def get_value(self, id_num):
+        address = self.get_address(id_num)
+        for item in self._buckets[address]:
+            if item.id_num == id_num:
+                return item.name
+        return None
